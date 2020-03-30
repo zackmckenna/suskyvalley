8515b851d5b20a6d942a6ca38daf75a341a9d1af
Zack McKenna <zackmckenna@gmail.com>
2020-03-30 14:44:59 -0400

    Updates

---
+++ precache-manifest.3edad9310417ad52b209d9daccbbef8e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe2b18f0a1ad1d3b03d695f558073c7",
+    "url": "/github.io/suskyvalley/index.html"
+  },
+  {
+    "revision": "2308b68cae36c9df8572",
+    "url": "/github.io/suskyvalley/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cb12bdb869a4584e9b2c",
+    "url": "/github.io/suskyvalley/static/css/main.7f287089.chunk.css"
+  },
+  {
+    "revision": "2308b68cae36c9df8572",
+    "url": "/github.io/suskyvalley/static/js/2.dbb8f4a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github.io/suskyvalley/static/js/2.dbb8f4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb12bdb869a4584e9b2c",
+    "url": "/github.io/suskyvalley/static/js/main.e5e681dc.chunk.js"
+  },
+  {
+    "revision": "2c27aea7c5b8c98a24af",
+    "url": "/github.io/suskyvalley/static/js/runtime-main.2cf2ab7a.js"
+  },
+  {
+    "revision": "d52e9a7370b1ca51e6c58f85e96c45d5",
+    "url": "/github.io/suskyvalley/static/media/StardewValley.d52e9a73.ttf"
+  },
+  {
+    "revision": "1b1432fe0f6a48c9122c20f307e6737d",
+    "url": "/github.io/suskyvalley/static/media/StardewValleyCaps.1b1432fe.ttf"
+  },
+  {
+    "revision": "790b1019257d2ed7ade649d651854fea",
+    "url": "/github.io/suskyvalley/static/media/StardewValleyText.790b1019.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/suskyvalley/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4b9ac233d5a7001190695a827a351ab",
+    "url": "/github.io/suskyvalley/static/media/sdv_divider.e4b9ac23.png"
+  },
+  {
+    "revision": "317d78439a928358e441775cdd159315",
+    "url": "/github.io/suskyvalley/static/media/stardewbackground.317d7843.png"
+  }
+]);
